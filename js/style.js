82bd846af9106fd2d44f313840f5dd90783f2e42
venika-sem <99354917+venika-sem@users.noreply.github.com>
2022-03-09 12:47:39 +0000

    added index.html

---
+++ js/style.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 Venika Sem All rights reserved
+//
+// Created by: Venika Sem
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  // this function displays "Hello, World!"
+
+  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>";
+}
